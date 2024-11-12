a22a9c5c30b040fbc0d44631c9156a0d6720a3ad
FlakeyCheese <rgblackett@gmail.com>
2024-11-12 14:34:53 +0000

    Add project files.

---
+++ stackOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using stackOOP;
+using System.Collections;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        StackClass stack1 = new StackClass(52);//create a stack for 52 items
+        Console.WriteLine("We have {0} stacks now", StackClass.stackCount);//using the static stackCount variable.
+        StackClass stack2 = new StackClass(10);//create a stack for 10 items
+
+        stack1.push("A");
+        stack1.push("B");
+        Console.WriteLine(stack1.peek());
+        stack2.push("C");
+        Console.WriteLine(stack1.pop());
+        Console.WriteLine(stack2.pop());
+        Console.WriteLine(stack2.pop());
+        Console.WriteLine("We have {0} stacks now", StackClass.stackCount);
+        StackClass stack3 = new StackClass(20);
+        Console.WriteLine("We have {0} stacks now",StackClass.stackCount);
+    }
+}
---
+++ stackOOP/StackClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stackOOP
+{
+    internal class StackClass
+    {
+        private int top;
+        private object[] stack;
+        public static int stackCount = 0;
+
+        public StackClass(int stack_size)
+        {
+            top = -1;
+            stack = new string[stack_size];
+            stackCount++;
+            Console.WriteLine("new stack created with {0} spaces", stack_size);
+        }
+
+        private bool is_empty()
+        {
+            if (top == -1)
+            { return true; }
+            else
+            { return false; }
+        }
+        private bool is_full()
+        {
+            if (top == 51)
+            { return true; }
+            else
+            { return false; }
+        }
+
+        public object peek( )
+        {
+            if (this.is_empty())
+            {
+                Console.WriteLine("The stack is empty");
+                return null;//nothing to return
+            }
+            else
+            { return stack[top]; }
+        }
+
+        public void push(string data)
+        {
+            if (this.is_full())
+            { Console.WriteLine("The Stack is full - unable to push"); }
+            else
+            {
+                top++;
+                stack[top] = data;
+            }
+        }
+
+        public object pop()
+        {
+            if (is_empty())
+            {
+                Console.WriteLine("Stack is empty - nothing to pop");
+                return null;//nothing to return
+            }            
+            top--;//move the top down
+            return stack[top+1];//return the item that was at the top
+        }
+    }
+}
